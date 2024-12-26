c525f9da9ff64df20375a6139a22ff659fe9c36b
Jjass28766 <ongtangcog@gmail.com>
2024-12-26 19:50:47 +0800

    Update BRIXMODZ PERSONAL USE (1).lua

--- BRIXMODZ PERSONAL USE (1).lua
+++ BRIXMODZ PERSONAL USE (1).lua
@@ -224,7 +224,7 @@ HexPatches.BrixMODZ("libanogs.so", 0x24282C, "h 00 00 80 D2 C0 03 5F D6", 32);
 HexPatches.BrixMODZ("libanogs.so", 0x242994, "h 00 00 80 D2 C0 03 5F D6", 32);
 HexPatches.BrixMODZ("libanogs.so", 0x242B58, "h 00 00 80 D2 C0 03 5F D6", 32);
 HexPatches.BrixMODZ("libanogs.so", 0x2442C8, "h 00 00 80 D2 C0 03 5F D6", 32);
-HexPatches.BrixMODZ("libanogs.so", 0x22C763, "h 00 00 80 D2 C0 03 5F D6", 32);
+HexPatches.BrixMODZ("libanogs.so", 0x1F0C54, "h 00 00 80 D2 C0 03 5F D6", 32);
 HexPatches.BrixMODZ("libanogs.so", 0x1749A8, "h 00 00 80 D2 C0 03 5F D6", 32);
 gg.toast("BYPASS AND ANTI CRASH ACTIVATED")
 end

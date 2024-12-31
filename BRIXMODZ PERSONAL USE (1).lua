1f3a7bcfa7824ff9d0324634aeee659d11338d21
Jjass28766 <ongtangcog@gmail.com>
2025-01-01 02:50:26 +0800

    Update BRIXMODZ PERSONAL USE (1).lua

--- BRIXMODZ PERSONAL USE (1).lua
+++ BRIXMODZ PERSONAL USE (1).lua
@@ -637,14 +637,14 @@ end
 
 function box4()
 local so = gg.getRangesList('libunity.so')[1].start
-local py = 0x7BB162C
-gg.setValues({{address = so + py, flags = gg.TYPE_QWORD, value = "hC00080D2C0035FD6"}})
+local py = 0x7BB1514
+gg.setValues({{address = so + py, flags = gg.TYPE_QWORD, value = "h200080D2C0035FD6"}})
 local so = gg.getRangesList('libunity.so')[1].start
-local py = 0x7BB2174
-gg.setValues({{address = so + py, flags = gg.TYPE_QWORD, value = "hC00080D2C0035FD6"}})
+local py = 0x7BB1C28
+gg.setValues({{address = so + py, flags = gg.TYPE_QWORD, value = "h200080D2C0035FD6"}})
 local so = gg.getRangesList('libunity.so')[1].start
-local py = 0x7BB11F8
-gg.setValues({{address = so + py, flags = gg.TYPE_QWORD, value = "hC00080D2C0035FD6"}})
+local py = 0x7BB1BD0
+gg.setValues({{address = so + py, flags = gg.TYPE_QWORD, value = "h200080D2C0035FD6"}})
 gg.toast("SMOOTH FPS ACTIVATED")
 end
 

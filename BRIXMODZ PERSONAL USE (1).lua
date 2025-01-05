21d478f1cec55abe5ea82d2bbd1bccb4b4b8173f
Jjass28766 <ongtangcog@gmail.com>
2025-01-05 21:35:04 +0800

    Update BRIXMODZ PERSONAL USE (1).lua

--- BRIXMODZ PERSONAL USE (1).lua
+++ BRIXMODZ PERSONAL USE (1).lua
@@ -637,6 +637,15 @@ end
 
 function box4()
 local so = gg.getRangesList('libunity.so')[1].start
+local py = 0x7BB162C
+gg.setValues({{address = so + py, flags = gg.TYPE_QWORD, value = "hC00080D2C0035FD6"}})
+local so = gg.getRangesList('libunity.so')[1].start
+local py = 0x7BB2174
+gg.setValues({{address = so + py, flags = gg.TYPE_QWORD, value = "hC00080D2C0035FD6"}})
+local so = gg.getRangesList('libunity.so')[1].start
+local py = 0x7BB11F8
+gg.setValues({{address = so + py, flags = gg.TYPE_QWORD, value = "hC00080D2C0035FD6"}})
+local so = gg.getRangesList('libunity.so')[1].start
 local py = 0x7BB1514
 gg.setValues({{address = so + py, flags = gg.TYPE_QWORD, value = "h200080D2C0035FD6"}})
 local so = gg.getRangesList('libunity.so')[1].start
